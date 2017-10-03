bd790ce66de43b72b163061fdd405bc9ee666222
tankedshoe <tankedshoe@gmail.com>
2017-10-03 09:10:14 -0600

    Fixed an error in the runner.

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -1,6 +1,22 @@
 package useful.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+import useful.model.Donut; //Must import as its from a different package.
+
 public class ToolController
 {
-
+	private List<Donut> donutList; //makes a list
+	
+	public ToolController()
+	{
+		donutList = new ArrayList<Donut>(); //instantiates a list
+	}
+	
+	public void start()
+	{
+		Donut temp = new Donut();
+		
+		donutList.add(temp);
+	}
 }
--- src/useful/controller/ToolRunner.java
+++ src/useful/controller/ToolRunner.java
@@ -4,7 +4,7 @@ public class ToolRunner
 {
 	public static void main(String[] args)
 	{
-		
+		//() calls something.
 		ToolController myApp = new ToolController();
 		myApp.start();
 		
