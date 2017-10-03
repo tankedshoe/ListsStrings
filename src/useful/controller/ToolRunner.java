bed486a50a24a082416c9037a695b91124145986
tankedshoe <tankedshoe@gmail.com>
2017-10-03 08:43:44 -0600

    Added a donut class.

--- src/useful/controller/ToolRunner.java
+++ src/useful/controller/ToolRunner.java
@@ -2,5 +2,10 @@ package useful.controller;
 
 public class ToolRunner
 {
-
+	public static void main(String [] args)
+	{
+		//() is a call.
+		ToolController myApp = new ToolController();
+		myApp.start();
+	}
 }
